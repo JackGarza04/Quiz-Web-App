caa598a98a2a8722fb5806e7e5167be1fabe6754
P1 <jack@garzafamily.org>
2022-01-31 09:55:10 -0800

    Change

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
 	$("#btnrev").click(function(){
 		$("#ans").css("visibility", "visible")
 	});
+  $("button").click(function(){
+	  if($("#test").val() == "6"){
+		alert("Correct!" + $("#test").val());
+	  }
+	  else{
+		alert("Sorry, wrong answer!");
+	  }
+  });
 });
