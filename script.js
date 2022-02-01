bc6a4a892960017e0a7f6b992b05597ce3272ac4
P1 <jack@garzafamily.org>
2022-02-01 09:53:27 -0800

    Changes

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
 	$("#check1").click(function(){
 		if($("#ans1").val() == "2x^2 + 2x + C"){
-			$("#w1").css("visibility", "hidden")
-			$("#r1").css("visibility", "visible")
+			$("#w1").css("visibility", "hidden").fadeTo(1, 0.01)
+			$("#r1").css("visibility", "visible").fadeTo(1000, 1.0)
 		}
 		else{
-			$("#r1").css("visibility", "hidden")
-			$("#w1").css("visibility", "visible")
+			$("#r1").css("visibility", "hidden").fadeTo(1, 0.01)
+			$("#w1").css("visibility", "visible").fadeTo(1000, 1.0)
 		}
 	});
   
 	$("#check2").click(function(){
-		if($("#ans2").val() == "12"){
-			$("#w2").css("visibility", "hidden")
-			$("#r2").css("visibility", "visible")
+		if($("#ans2").val() == "36x^2 + 14x + 5"){
+			$("#w2").css("visibility", "hidden").fadeTo(1, 0.01)
+			$("#r2").css("visibility", "visible").fadeTo(1000, 1.0)
 		}
 		else{
-			$("#r2").css("visibility", "hidden")
-			$("#w2").css("visibility", "visible")
+			$("#r2").css("visibility", "hidden").fadeTo(1, 0.01)
+			$("#w2").css("visibility", "visible").fadeTo(1000, 1.0)
+		}
+	});
+	
+	$("#check3").click(function(){
+		if($("#ans3").val() == "36x^2 + 14x + 5"){
+			$("#w3").css("visibility", "hidden").fadeTo(1, 0.01)
+			$("#r3").css("visibility", "visible").fadeTo(1000, 1.0)
+		}
+		else{
+			$("#r3").css("visibility", "hidden").fadeTo(1, 0.01)
+			$("#w3").css("visibility", "visible").fadeTo(1000, 1.0)
 		}
 	});
 });
