80e7bff34c679bf0cb128e27b92810f5d194003d
P1 <jack@garzafamily.org>
2022-02-01 08:55:59 -0800

    Changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-	$("#btnrev").click(function(){
+	/* $("#submit").click(function(){
 		$("#ans").css("visibility", "visible")
-	});
+	}); */
   $("button").click(function(){
 	  if($("#test").val() == "6"){
-		alert("Correct!" + $("#test").val());
+		alert("Correct, " + $("#test").val() + " is the right answer");
 	  }
 	  else{
-		alert("Sorry, wrong answer!");
+		alert("Sorry, wrong answer");
 	  }
   });
 });
