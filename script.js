f74f582e001096ecd0422dcb78113f68a8c5bf3d
P1 <jack@garzafamily.org>
2022-02-01 09:32:29 -0800

    Good changes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-	/* $("#submit").click(function(){
-		$("#ans").css("visibility", "visible")
-	}); */
-  $("button").click(function(){
-	  if($("#test").val() == "6"){
-		alert("Correct, " + $("#test").val() + " is the right answer");
-	  }
-	  else{
-		alert("Sorry, wrong answer");
-	  }
-  });
+	$("#check1").click(function(){
+		if($("#ans1").val() == "2x^2 + 2x + C"){
+			$("#w1").css("visibility", "hidden")
+			$("#r1").css("visibility", "visible")
+		}
+		else{
+			$("#r1").css("visibility", "hidden")
+			$("#w1").css("visibility", "visible")
+		}
+	});
+  
+	$("#check2").click(function(){
+		if($("#ans2").val() == "12"){
+			$("#w2").css("visibility", "hidden")
+			$("#r2").css("visibility", "visible")
+		}
+		else{
+			$("#r2").css("visibility", "hidden")
+			$("#w2").css("visibility", "visible")
+		}
+	});
 });
