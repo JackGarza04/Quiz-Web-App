7fcc20baa266190d6d7e6fc8f01f924ef6931ace
JackGarza04 <jack@garzafamily.org>
2022-02-03 00:13:55 -0800

    Condensed JS functions
    Optimized the JS file and added CSS

--- script.js
+++ script.js
@@ -1,48 +1,24 @@
 $(document).ready(function() {
 	
-	/* add for loop */
-	$("#check1").click(function() {
-		checkAnswer("#userAns1", "2x^2 + 2x + C", "#feedback1")
-	});
-
-	$("#check2").click(function() {
-		checkAnswer("#userAns2", "36x^2 + 14x + 5", "#feedback2")
-	});
-
-	$("#check3").click(function() {
-		checkAnswer("#userAns3", "6x^3 + 3x^2 + 3x + C", "#feedback3")
-	});
-
-	$("#check4").click(function() {
-		checkAnswer("#userAns4", "3e^x + C", "#feedback4")
-	});
-  
-	$("#check5").click(function() {
-		checkAnswer("#userAns5", "5x + C", "#feedback5")
-	});
-  
-	$("#check6").click(function() {
-		checkAnswer("#userAns6", "2cos(2x)", "#feedback6")
-	});
-  
-	$("#check7").click(function() {
-		checkAnswer("#userAns7", "6x^8", "#feedback7")
-	});
-  
-	$("#check8").click(function() {
-		checkAnswer("#userAns8", "16x^3 + 8x", "#feedback8")
-	});
-  
-	$("#check9").click(function() {
-		checkAnswer("#userAns9", "e^(x + 1)", "#feedback9")
-	});
-  
-	$("#check10").click(function() {
-		checkAnswer("#userAns10", "y = 2x - 13", "#feedback10")
-	});
+	let answerKey = ["2x^2 + 2x + C", 
+					 "36x^2 + 14x + 5", 
+					 "6x^3 + 3x^2 + 3x + C", 
+					 "3e^x + C", 
+					 "5x + C", 
+					 "2cos(2x)", 
+					 "6x^8", 
+					 "16x^3 + 8x", 
+					 "e^(x + 1)", 
+					 "y = 2x - 13"];
+	
+	for(let n = 1; n <= 10; n++) {
+		$("#check"+n).click(function() {
+			checkAnswer("#userAns"+n, answerKey[n-1], "#feedback"+n)
+		});
+	}
 	
 	function checkAnswer(userAnswer, correctAnswer, feedback) {
-		
+		//Function checks user answer to matching answerkey answer w/o spaces
 		if($(userAnswer).val().replace(/\s+/g, "") == correctAnswer.replace(/\s+/g, "")){
 			$(feedback).removeClass("wrong").addClass("right").fadeTo(1, 0.0).text("Correct!").fadeTo(1000, 1.0)
 		}
